df10b97c2307d0dd0cd03b46c27d686213a2edcb
nelinory <15143882+nelinory@users.noreply.github.com>
2023-07-02 18:33:01 -0500

    Resolves #12 , Resolves #6

---
+++ Messages/ProgressTrackActionMessage.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace SupernoteDesktopClient.Messages
+{
+    public class ProgressTrackActionMessage : ValueChangedMessage<bool>
+    {
+        public ProgressTrackActionMessage(bool value) : base(value) { }
+    }
+}
--- Models/FileSystemObjectInfo.cs
+++ Models/FileSystemObjectInfo.cs
@@ -9,6 +9,12 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows.Media;
+using SupernoteSharp.Business;
+using SupernoteSharp.Common;
+using SupernoteSharp.Entities;
+using CommunityToolkit.Mvvm.Messaging;
+using SupernoteDesktopClient.Messages;
+using System.Threading.Tasks;
 
 namespace SupernoteDesktopClient.Models
 {
@@ -54,22 +60,55 @@ namespace SupernoteDesktopClient.Models
         }
 
         [RelayCommand]
-        private void OnOpenSelectedItem(object parameter)
+        private async Task OnOpenSelectedItem(object parameter)
+        {
+            await Task.Run(() => ConvertNoteDocument(parameter));
+        }
+
+        private void ConvertNoteDocument(object parameter)
         {
             FileSystemObjectInfo item = parameter as FileSystemObjectInfo;
             if ((item.FileSystemInfo.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
                 return;
 
-            ProcessStartInfo psi = new ProcessStartInfo();
-            psi.FileName = item.FileSystemInfo.FullName;
-            psi.UseShellExecute = true;
             try
             {
+                WeakReferenceMessenger.Default.Send(new ProgressTrackActionMessage(true)); // action started
+
+                string selectedItemFullName = item.FileSystemInfo.FullName;
+
+                if (item.FileSystemInfo.Extension == ".note" && item.FileSystemInfo.Exists == true)
+                {
+                    selectedItemFullName = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(item.FileSystemInfo.Name) + "_sdc.pdf");
+
+                    using (FileStream fileStream = new FileStream(item.FileSystemInfo.FullName, FileMode.Open, FileAccess.Read))
+                    {
+                        Parser parser = new Parser();
+                        Notebook notebook = parser.LoadNotebook(fileStream, Policy.Strict);
+                        Converter.PdfConverter converter = new Converter.PdfConverter(notebook, DefaultColorPalette.Grayscale);
+
+                        // convert all pages to vector PDF and build all links
+                        byte[] allPages = converter.ConvertAll(vectorize: true, enableLinks: true);
+                        // save the result
+                        File.WriteAllBytes(selectedItemFullName, allPages);
+                    }
+                }
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = selectedItemFullName,
+                    UseShellExecute = true
+                };
+
                 Process process = Process.Start(psi);
             }
             catch (Exception)
             {
                 // TODO: Error handling
+            }
+            finally
+            {
+                WeakReferenceMessenger.Default.Send(new ProgressTrackActionMessage(false)); // action completed
             }
         }
 
