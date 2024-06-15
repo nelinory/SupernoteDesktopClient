83356c659531894ed4bda5f550a4f4c3f0f71718
nelinory <15143882+nelinory@users.noreply.github.com>
2024-06-15 14:26:23 -0500

    Preparing for release 0.8

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyVersion("0.8.*")]
 [assembly: AssemblyTitle("Supernote Desktop Client")]
 [assembly: AssemblyProduct("Supernote Desktop Client")]
 [assembly: AssemblyCopyright("Copyright © nelinory 2024")]
